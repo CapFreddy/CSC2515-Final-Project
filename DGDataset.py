135ec2eb5c624fbf0aba1ac46f5a49d96e1d411d
Huakun Shen <huakun.shen@huakunshen.com>
2022-11-16 00:37:06 -0500

    Add supporting some files, refactored DGDataset

--- DGDataset.py
+++ DGDataset.py
@@ -1,7 +1,9 @@
 import bisect
-from torchvision import datasets
+from typing import List
+from torchvision.datasets import ImageFolder
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
+
 
 class DGDataset(Dataset):
     """
@@ -15,7 +17,7 @@ class DGDataset(Dataset):
     """
 
     @staticmethod
-    def cumsum(sequence):
+    def cumsum(sequence: List[ImageFolder]) -> List[int]:
         r, s = [], 0
         for e in sequence:
             l = len(e)
@@ -23,27 +25,27 @@ class DGDataset(Dataset):
             s += l
         return r
 
-    def __init__(self, dataroots, mode="train"):
+    def __init__(self, dataroots: List[str], mode: str = "train"):
         super(DGDataset, self).__init__()
         assert len(dataroots) > 0, 'datasets should not be an empty iterable'
-        self.datasets = []
-        for dataroot in dataroots:
-            self.datasets.append(datasets.ImageFolder(
-                root="../DataSets/digits_dg/" + dataroot + "/" + mode,
-                transform=transforms.Compose(
-                    [transforms.Resize(32), transforms.ToTensor(),
-                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-                )))
+        self.datasets = [ImageFolder(
+            root=f"../DataSets/digits_dg/{dataroot}/{mode}",
+            transform=transforms.Compose(
+                [transforms.Resize(32), transforms.ToTensor(),
+                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+            ))
+            for dataroot in dataroots]
         self.cumulative_sizes = self.cumsum(self.datasets)
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """Get the last number in cummulative sizes, which should be the number of images in all datasets
+
+        :return: size of this ensemble dataset (number of images)
+        :rtype: int
+        """
         return self.cumulative_sizes[-1]
 
     def __getitem__(self, idx):
         dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
-        if dataset_idx == 0:
-            sample_idx = idx
-        else:
-            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
-        test = self.datasets[dataset_idx][sample_idx]
+        sample_idx = idx - (0 if dataset_idx == 0 else self.cumulative_sizes[dataset_idx - 1])
         return self.datasets[dataset_idx][sample_idx], dataset_idx
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# CSC2515-Final-Project
+# CSC2515 Final Project
+
+## Dataset
+
+Download dataset from https://drive.google.com/file/d/15V7EsHfCcfbKgsDmzQKj_DfXt_XYp_P7/view?pli=1 and store it to `dataset/digits_dg.zip`.
+
+Then run `make extract_dataset`
